78e0cfdcf109baa70d513c7f93f895a547f72187
JTTran <tranjt80>
2020-11-09 10:35:04 +0200

    part4 ex4.5 favorite blog

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,27 @@ const totalLikes = (blogs) => {
   return blogs.reduce((totalLikes, blog) => totalLikes + blog.likes, 0)
 }
 
+function isEmpty(obj) {
+  return Object.keys(obj).length === 0
+}
+
+const favoriteBlog = (blogs) => {
+  let topBlog = {}
+
+  blogs.forEach(blog => {
+    if (isEmpty(topBlog)) topBlog = blog
+    else if (topBlog.likes < blog.likes) topBlog = blog
+  })
+
+  return {
+    title: topBlog.title,
+    author: topBlog.author,
+    likes: topBlog.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
