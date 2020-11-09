0b0e8f8b25960557f82e1593b5aae4bc70c67d5d
JTTran <tranjt80>
2020-11-09 07:08:35 +0200

    part4 ex4.3 and implemented eslint

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ const blogsRouter = require('./controllers/blogs')
 
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(() => {
-        logger.info('connected to MongoDB')
-    })
-    .catch((error) => {
-        logger.error('error connection to MongoDB:', error.message)
-    })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.json())
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,46 +3,46 @@ const Blog = require('../models/blog')
 
 
 blogsRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
 })
 
 
 blogsRouter.post('/', (request, response) => {
-    const body = request.body
+  const body = request.body
 
-    if (!body) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    } else if (!body.title) {
-        return response.status(400).json({
-            error: 'title missing'
-        })
-    } else if (!body.author) {
-        return response.status(400).json({
-            error: 'author missing'
-        })
-    } else if (!body.url) {
-        return response.status(400).json({
-            error: 'url missing'
-        })
-    } else if (!body.likes) {
-        return response.status(400).json({
-            error: 'likes missing'
-        })
-    }
+  if (!body) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  } else if (!body.title) {
+    return response.status(400).json({
+      error: 'title missing'
+    })
+  } else if (!body.author) {
+    return response.status(400).json({
+      error: 'author missing'
+    })
+  } else if (!body.url) {
+    return response.status(400).json({
+      error: 'url missing'
+    })
+  } else if (!body.likes) {
+    return response.status(400).json({
+      error: 'likes missing'
+    })
+  }
 
-    const blog = new Blog(body)
+  const blog = new Blog(body)
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
 })
 
 module.exports = blogsRouter
